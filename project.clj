731648a16051078525f830c088632fd8c423cf6f
Priyadarshi Raj <praj-foss@users.noreply.github.com>
2019-12-10 01:19:57 +0530

    Add basic API
    - Provide functions to read ID3v2 header
    - Add lein config and other files

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject coucal "0.0.1-SNAPSHOT"
+  :description "A Clojure library for parsing ID3 tags"
+  :url "https://github.com/praj-foss/coucal"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
