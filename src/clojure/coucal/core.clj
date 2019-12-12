2a80343049e9eff2c5a5e6aac0bf43db9790cbf0
Priyadarshi Raj <praj-foss@users.noreply.github.com>
2019-12-12 09:34:25 +0530

    Create separate namespace for codecs
    - Create coucal.codec namespace
    - Check :magic-number is 'ID3'

--- src/clojure/coucal/core.clj
+++ src/clojure/coucal/core.clj
@@ -1,27 +1,9 @@
 (ns coucal.core
-  (:require [org.clojars.smee.binary.core :as bin]
-            [clojure.java.io :as io])
-  (:import  [coucal.util BitUtil]))
+  (:require [coucal.codec :as codec]
+            [clojure.java.io :as io]
+            [org.clojars.smee.binary.core :refer [decode]]))
 
-(def :synchsafe
-  (bin/compile-codec :int-be #(BitUtil/synchsafe %) #(BitUtil/unsynchsafe %)))
-
-(def ^:private header
-  (bin/ordered-map
-    :magic-number (bin/string "ISO-8859-1" :length 3) ;; "ID3"
-    :version      (bin/ordered-map
-                    :major :byte
-                    :minor :byte)
-    :flags        (bin/bits [nil nil nil nil
-                             :footer?
-                             :experimental?
-                             :extended-header?
-                             :unsynchronised?])
-    :tag-size     :synchsafe))
-
-(def ^:private mp3-codec (bin/compile-codec [header]))
-
-(defn read-mp3
+(defn read-tag
   [path]
   (with-open [in (io/input-stream path)]
-    (bin/decode mp3-codec in)))
+    (decode codec/tag in)))
