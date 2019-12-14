dd5c875ad72df7e99f3a8df8470aa3087a1066b5
Priyadarshi Raj <praj-foss@users.noreply.github.com>
2019-12-14 22:03:19 +0530

    Support frames
    - Store frame ids in a vector
    - Use byte arrays in ReadUtil

--- src/clojure/coucal/core.clj
+++ src/clojure/coucal/core.clj
@@ -6,6 +6,7 @@
 ;; Aliases
 (defn- read-unsigned-int  [stream] (ReadUtil/unsignedInt stream))
 (defn- read-synchsafe-int [stream] (ReadUtil/synchsafeInt stream))
+(defn- read-str [stream size] (ReadUtil/string stream size))
 
 (defn- read-version
   [stream]
@@ -14,49 +15,58 @@
 
 (defn- read-flags
   [stream]
-  (let [flags    (.read stream)
-        when-bit #(bit-test flags %)]
+  (let [flag-byte (.read stream)]
     (cond-> #{}
-            (when-bit 5) (conj :experimental?)
-            (when-bit 6) (conj :extended?)
-            (when-bit 7) (conj :unsynchronised?))))
+            (bit-test flag-byte 5) (conj :experimental?)
+            (bit-test flag-byte 6) (conj :extended?)
+            (bit-test flag-byte 7) (conj :unsynchronised?))))
 
 (defn- read-extra-flags
   [stream]
-  (let [flags-1  (.read stream)
-        _        (.read stream)                             ;; TODO: Make use
-        when-bit #(bit-test %1 %2)]
-    (cond-> #{}
-            (when-bit flags-1 7) (conj :crc?))))
+  (let [flag-byte (.read stream)
+        _         (.read stream)]                           ;; TODO: Make use
+    (cond-> []
+            (bit-test flag-byte 7) (conj :crc?))))
 
-(defn- read-extended
-  [stream]
-  (as-> {} extended
-        (assoc extended :size (read-unsigned-int stream))
-        (assoc extended :flags (read-extra-flags stream))
-        (assoc extended :padding (read-unsigned-int stream))
-        (conj extended
-              (if (:crc? (:flags extended))
-                {:crc (read-unsigned-int stream)}))))
+(defn- extend-header
+  [header stream]
+  (if (:extended? (:flags header))
+    (-> header
+        (update :remaining -
+                (+ 4 (read-unsigned-int stream)))           ;; Cut extended header size
+        (update :flags into (read-extra-flags stream))
+        (update :remaining + (read-unsigned-int stream))    ;; Add padding size
+        (conj (if (:crc? (:flags header))
+                {:crc (read-unsigned-int stream)})))
+    header))
 
 (defn- read-header
   [^InputStream stream]
-  (if-not (->> #(.read stream)
-               (repeatedly 3)
-               (map char)
-               (= [\I \D \3]))
+  (if (= "ID3" (read-str stream 3))
+    (-> {}
+        (assoc :version (read-version stream))
+        (assoc :flags (read-flags stream))
+        (assoc :remaining (read-synchsafe-int stream))
+        (extend-header stream))
     (throw (ex-info "Invalid ID3v2 tag"
-                    {:cause "Magic number not found"}))
-    (as-> {} header
-          (assoc header :version (read-version stream))
-          (assoc header :flags (read-flags stream))
-          (assoc header :size (read-synchsafe-int stream))
-          (conj header
-                (if (:extended? (:flags header))
-                  (select-keys (read-extended stream)
-                               [:padding :crc]))))))
+                    {:cause "Magic number not found"}))))
+
+(defn- read-frames
+  [tag stream]
+  (if-not (> (:remaining tag) 0)
+    tag
+    (let [id   (read-str stream 4)
+          size (read-unsigned-int stream)
+          _    (.skip stream (+ 2 size))]
+      (-> tag
+          (update :frames conj id)
+          (update :remaining - (+ 10 size))
+          (recur stream)))))
 
 (defn read-tag
   [path]
   (with-open [stream (io/input-stream path)]
-    (read-header stream)))
+    (-> (read-header stream)
+        (assoc :frames [])
+        (read-frames stream)
+        (dissoc :remaining))))
--- src/java/coucal/util/ReadUtil.java
+++ src/java/coucal/util/ReadUtil.java
@@ -2,22 +2,36 @@ package coucal.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import static java.lang.Byte.toUnsignedLong;
 
 /**
  * Common utilities for reading from {@link InputStream}.
  */
 public final class ReadUtil {
     public static long unsignedInt(final InputStream stream) throws IOException {
-        return    stream.read() << 24
-                | stream.read() << 16
-                | stream.read() << 8
-                | stream.read();
+        final byte[] bytes = readBytes(stream, 4);
+        return    toUnsignedLong(bytes[0]) << 24
+                | toUnsignedLong(bytes[1]) << 16
+                | toUnsignedLong(bytes[2]) << 8
+                | toUnsignedLong(bytes[3]);
     }
 
     public static long synchsafeInt(final InputStream stream) throws IOException {
-        return    stream.read() << 21
-                | stream.read() << 14
-                | stream.read() << 7
-                | stream.read();
+        final byte[] bytes = readBytes(stream, 4);
+        return    toUnsignedLong(bytes[0]) << 21
+                | toUnsignedLong(bytes[1]) << 14
+                | toUnsignedLong(bytes[2]) << 7
+                | toUnsignedLong(bytes[3]);
+    }
+
+    public static String string(final InputStream stream, final int size) throws IOException {
+        return new String(readBytes(stream, size), StandardCharsets.ISO_8859_1);
+    }
+
+    private static byte[] readBytes(final InputStream stream, final int size) throws IOException{
+        final byte[] bytes = new byte[size];
+        stream.read(bytes);
+        return bytes;
     }
 }
